5bc3c3eb73f75a9c07bbeba654ead5e4e950c3db
nebojsa0511 <nmsvilajnac@gmail.com>
2016-10-17 14:35:15 +0200

    aaaa
    individualno poveyivanje

--- conn.php
+++ conn.php
@@ -6,10 +6,10 @@ $dbpass=""; //your database password
 $db = @ mysql_connect("localhost","$dbuser","$dbpass");
 @ mysql_select_db("$dbname",$db);
 
-$domain = "edpudol.com"; //Enter your domain here for SEO purpose only
-$siteurl ="http://localhost/cms"; // please specify your site url
+$domain = "neoart.rs"; //Enter your domain here for SEO purpose only
+$siteurl ="http://localhost/cmsjoke/CMSJoke"; // please specify your site url
 
-$pubId="pub-5071570080813531";
+$pubId="";
 $url="http://localhost/cms"; // no / at the end of url
 $joketitle="Mala Sala od sajta"; //put your joke site title here... will show at the top of the site.
 
